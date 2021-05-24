0dacb4b112203d600b238f835ddbdbef06182d06
Student <o1083012@gm.pu.edu.tw>
2021-05-24 14:13:01 +0800

    2021上機考v4_(韓婉晶)

--- app/src/main/java/tw/edu/pu/o1083012/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083012/crazyshape/MainActivity.kt
@@ -47,7 +47,8 @@ class MainActivity : AppCompatActivity() {
                         return true
                     }
                 })
+                return true
             }
-        }
+        })
     }
 }
