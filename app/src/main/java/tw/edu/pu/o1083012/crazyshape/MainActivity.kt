7d69ac3600f684dbd69e32669aba45037e778837
Student <o1083012@gm.pu.edu.tw>
2021-05-24 13:32:02 +0800

    2021上機考v2_(韓婉晶)

--- app/src/main/java/tw/edu/pu/o1083012/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083012/crazyshape/MainActivity.kt
@@ -4,23 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(baseContext, "作者：韓婉晶", Toast.LENGTH_LONG).show()
+        val img: ImageView = findViewById(R.id.imgTitle)
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800,600)
+            .into(img)
 
-        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
-            override fun onLongClick(p0: View?): Boolean {
+
+        Toast.makeText(baseContext, "作者：韓婉晶", Toast.LENGTH_LONG).show()
+        imgNext.setOnLongClickListener(object: View.OnLongClickListener{
+            override fun onLongClick(p0: View): Boolean {
                 intent = Intent(this@MainActivity, GameActivity::class.java)
                 startActivity(intent)
+
+
+
                 return true
             }
+
         })
     }
 }
+
 
